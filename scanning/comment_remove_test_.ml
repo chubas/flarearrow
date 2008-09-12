96af30627605b918291e86d1d8a12e09efde80d3
rogeliorv <rogeliorv@e7b0d6f7-5054-0410-bf6d-e59bcb6200f7>
2008-09-12 22:38:43 +0000

    added block separator

--- scanning/comment_remove_test_.ml
+++ scanning/comment_remove_test_.ml
@@ -6,7 +6,7 @@ open Flarelib;;
 let text_fixture ="Comment remove suite" >:::
 	[
 		"Test inicio" >:: (fun()->
-			assert_equal "<html>\n\t<head>\n\t\t<title></title>\n\t</head>\n\t<body>\n\t\ttext\n\t\t           \n\t\tmore                  text\n\t\t{{ {# does nothing #} }}\n\t</body>\n</html>\n"
+			assert_equal "<html>\r\n\t<head>\r\n\t\t<title></title>\r\n\t</head>\r\n\t<body>\r\n\t\ttext\r\n\t\t           \r\n\t\tmore                  text\r\n\t\t{{ {# does nothing #} }}\r\n\t</body>\r\n</html>\r\n"
 			(remove "comment_remove_test_one.html");
 			);
 		"Test error" >:: (fun()->
@@ -14,4 +14,4 @@ let text_fixture ="Comment remove suite" >:::
 			);
 	]
 	
-let _= run_test_tt ~verbose:true text_fixture;  
+let _= run_test_tt ~verbose:true text_fixture;   
