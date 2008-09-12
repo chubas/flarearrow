b8074d198bd42dd5062eab57af073e12aee2f4a3
rogeliorv <rogeliorv@e7b0d6f7-5054-0410-bf6d-e59bcb6200f7>
2008-09-12 00:59:38 +0000

    Altered the reading example, this time using a more decent method

--- trunk/scanning/file_reading.ml
+++ trunk/scanning/file_reading.ml
@@ -1,31 +1,30 @@
-(* NOTES: *)
-(* This method only works with files, this means channels like sockets and keyboard *)
-(* will not work. *)
+(* File name *)
+let filename = "file_reading.ml";;
+(* Open the file *)
+let reading_channel = open_in filename;;
+(* Create a 2048 character buffer *)
+let buf = Buffer.create 2048;;
 
-(* TODO: *)
-(* Check the Buffer module and read incrementally, this will help a lot in the*)
-(* scanning process *)
-(* Unit Tests for File reading !*) 
+(* Read line by line of the file in an expandable buffer *)
+let read () = 
+	let rec loop () = 
+	  (* Read a line *)
+	  let line = input_line reading_channel in
+	  (* Put the line in the buffer *)
+	  Buffer.add_string buf line;
+	  (* Put the newline character in the buffer *)
+	  Buffer.add_char buf '\n';
+	  (* Continue reading *)
+	  loop ()
+	  in
+	  (* Read the file, and return the contents of the buffer as string
+	     when EOF is reached *)
+	  try
+	    loop ()
+	  with
+	    (* Return the contents of the buffer as a string *)
+	    End_of_file -> Buffer.contents buf
+;;
 
-
-(* Copies and returns the content in the channel to a string *)
-let read_whole_channel channel = 
-  (* Get the total of characters using in_channel_length function *)
-	let total = in_channel_length channel in
-  (* Create a string with total characters length *)
-	let result = String.create total in
-  (* Put the contents in channel to the result string *)
-	really_input channel result 0 total;
-	result;;
-
-
-(* Open the file and put the contents in the channel *)
-let read_whole_file filename =
-  let channel = open_in filename in
-  read_whole_channel channel;;
-
-
-(* Get the filename and call the reading function *)
-let filename = Sys.argv.(1);;
-let str = read_whole_file filename;;
-Printf.printf "%d characters read from file %s\n" (String.length str) filename
+(* Test it *)
+print_endline (read ());;
