4b7c430bb6f4ee5ea953f11a902084c31c813375
chubas7 <chubas7@e7b0d6f7-5054-0410-bf6d-e59bcb6200f7>
2008-10-13 06:41:19 +0000

    MAJOR CHANGES HERE. Removed the shift/reduce conflicts in the grammar, made all operators functions and defined them in the file (Including concatenation operator for strings, now is "+"). Added implicit coertion for expressions to booleans (So you can make ugly things like python in which 0, , [], {} are false). Made lots of tests.

--- trunk/src/second_level_parser/expression_evaluator.ml
+++ trunk/src/second_level_parser/expression_evaluator.ml
@@ -1,9 +1,9 @@
+open Flarelib;;
 open Basic_types;;
 open Grammar;;
 open Second_level_parser;;
 open First_level_parser;;
 
-let string_of_char c = Printf.sprintf "%c" c;;
 (* Reduces type that eliminates functions to basic types *)
 type expression =
   | EXP  of slp_basic_type
@@ -12,10 +12,58 @@ type expression =
   | NULL
 ;;
 
-exception BoundFunctionError;;
-exception NotDeclaredFunctionError;;
+let type_of ?(strict = true) exp = match exp with
+  | EXP (Numeric n)         -> if strict then (
+    match n with
+      | Int _     -> "integer"
+      | Float _   -> "float"  
+   ) else
+    "numeric"
+  | EXP (Boolean _)         -> "boolean"
+  | EXP string_or_char      -> if strict then (
+    match string_or_char with
+      | String _  -> "string"
+      | Char _    -> "character"
+      | _         -> "unknown type" (* Should never get here *)
+  ) else
+    "string or character"
+  | LIST _                  -> "list"
+  | DICT _                  -> "dict"
+  | NULL                    -> "null"
+;;
+
+(*
+ * If a dict list contains more than a value for a
+ * single key, it just keeps the last one
+ *)  
+let sanitize_dict list_of_pairs =
+  let rec add_or_replace lst acum = match lst with
+    | [] -> acum
+    | (identifier, value)::tail -> if List.mem_assoc identifier acum then
+      add_or_replace tail acum
+    else
+      add_or_replace tail ((identifier, value)::acum)
+  in
+    add_or_replace (List.rev list_of_pairs) [] 
+;;
+
+exception BoundFunctionError of string;;
+exception NotDeclaredFunctionError of string;;
+
+                                  (* expected, given *)
 exception InvalidNumberOfParameters of (int * int);;
+
+                                (* position, expected *)
 exception InvalidParameterType of (int * string);;
+
+                            (* given, expected type *)
+exception BadArgumentError of (expression * string);;
+  
+                                    (* accesible, given, actual length *)
+exception IndexOutOfBoundsException of (expression * int * int);;
+
+                                      (* function, type *)
+exception NotSupportedTypeException of string * string;;
 exception UnknownException;;
 
 (* Hashtable that contains custom function definitions *)
@@ -29,7 +77,7 @@ let _P_FUNS_HASHTABLE = Hashtbl.create 50;;
  *)
 let _DEFINE_P_FUN name funct =
   if Hashtbl.mem _P_FUNS_HASHTABLE name then
-    raise BoundFunctionError
+    raise (BoundFunctionError name)
   else
     Hashtbl.add _P_FUNS_HASHTABLE name funct;;
 
@@ -41,7 +89,7 @@ let _DEFINE_P_FUN name funct =
 let rec _EVAL_P_FUN identifier params = (try
     (Hashtbl.find _P_FUNS_HASHTABLE identifier) (reduce_eval_list params)
   with 
-  | Not_found -> raise NotDeclaredFunctionError
+  | Not_found -> raise (NotDeclaredFunctionError identifier)
 )
 (* 
  * Transform from slp_ocamlets to expressions, with all
@@ -61,7 +109,7 @@ and reduce_eval_dict dict =
     | id, P_EXP (P_DICT  d) -> id, DICT (reduce_eval_dict d)
     | id, P_EXP (P_BASIC b) -> id, EXP b
     | id, P_FUN (str,  par) -> id, _EVAL_P_FUN str par
-  ) dict
+  ) (sanitize_dict dict)
 ;;
 
 (* Alias for reduce_eval_list *)
@@ -74,11 +122,206 @@ let rd = reduce_eval_dict;;
 (* Add standard functions *)
 (* ********************** *)
 
-_DEFINE_P_FUN "strlen" 
+_DEFINE_P_FUN "_plus"
+  (function
+    (* Numeric addition *)
+    | [EXP (Numeric (Int a)); EXP (Numeric (Int b))] -> 
+        EXP (Numeric (Int (a + b)))
+    | [EXP (Numeric (Int a)); EXP (Numeric (Float b))]
+    | [EXP (Numeric (Float b)); EXP (Numeric (Int a))] ->
+        EXP (Numeric (Float (b +. (float_of_int a))))
+    | [EXP (Numeric (Float a)); EXP (Numeric (Float b))] -> 
+        EXP (Numeric (Float (a +. b)))
+    
+    (* Literal addition *)
+    | [EXP (String s); EXP (String t)] ->
+        EXP (String (s ^ t))
+    | [EXP (String s); EXP (Char t)] ->
+        EXP (String (s ^ (string_of_char t)))
+    | [EXP (Char t); EXP (String s)] ->
+        EXP (String ((string_of_char t) ^ s))
+    | [EXP (Char s); EXP (Char t)] ->
+        EXP (String ((string_of_char s) ^ (string_of_char t)))
+   
+    (* List addition *)
+    | [LIST l; LIST m] ->
+        LIST (l @ m)
+    
+    (* Dict addition *)
+    | [DICT d; DICT e] ->
+        DICT (sanitize_dict (d @ e))
+    
+    | [param1; _] -> 
+        raise (InvalidParameterType (2, type_of param1 ~strict:false))
+    | other ->
+        raise (InvalidNumberOfParameters (2, List.length other)) 
+  )
+;;
+
+_DEFINE_P_FUN "_minus"
+  (function
+    (* Numeric substraction *)
+    | [EXP (Numeric (Int a)); EXP (Numeric (Int b))] -> 
+        EXP (Numeric (Int (a - b)))
+    | [EXP (Numeric (Int a)); EXP (Numeric (Float b))] ->
+        EXP (Numeric (Float ((float_of_int a) -. b)))
+    | [EXP (Numeric (Float a)); EXP (Numeric (Int b))] ->
+        EXP (Numeric (Float (a -. (float_of_int b))))
+    | [EXP (Numeric (Float a)); EXP (Numeric (Float b))] -> 
+        EXP (Numeric (Float (a -. b)))    
+    | [_; _] -> raise (InvalidParameterType (2, "numeric"))
+    | other -> raise (InvalidNumberOfParameters (2, List.length other)) 
+  )
+;;
+
+_DEFINE_P_FUN "_times"
+  (function
+    (* Numeric multiplication *)
+    | [EXP (Numeric (Int a)); EXP (Numeric (Int b))] -> 
+        EXP (Numeric (Int (a * b)))
+    | [EXP (Numeric (Int b)); EXP (Numeric (Float a))]
+    | [EXP (Numeric (Float a)); EXP (Numeric (Int b))] ->
+        EXP (Numeric (Float (a *. (float_of_int b))))
+    | [EXP (Numeric (Float a)); EXP (Numeric (Float b))] -> 
+        EXP (Numeric (Float (a *. b)))
+    (* String * int multiplication *)
+    | [EXP (Numeric (Int a)); EXP (String s)]
+    | [EXP (String s); EXP (Numeric (Int a))] -> 
+	      let rec repeat_string str n acum = match n with
+	        | 0 -> acum
+	        | n when n > 0 -> repeat_string str (n -1) (acum ^ str)
+	        | n -> raise (BadArgumentError ((EXP (Numeric (Int n))), "should not be negative"))
+	      in
+	       EXP (String (repeat_string s a ""))
+    (* Char * int multiplication *)
+    | [EXP (Numeric (Int a)); EXP (Char c)]
+    | [EXP (Char c); EXP (Numeric (Int a))] ->
+        EXP (String (String.make a c))
+    (* List * int multiplication *)
+    | [EXP (Numeric (Int a)); LIST l]
+    | [LIST l; EXP (Numeric (Int a))] ->
+        let rec repeat_list lst n acum = match n with
+	        | 0 -> acum
+	        | n when n > 0 -> repeat_list lst (n -1) (acum @ lst)
+	        | n -> raise (BadArgumentError ((EXP (Numeric (Int n))), "should not be negative"))
+	      in
+	       LIST (repeat_list l a [])
+    (* Exceptions with float and other types *)
+    | [EXP (Numeric (Float a)); _]
+      -> raise (InvalidParameterType (2, "float"))  
+    | [_; EXP (Numeric (Float a))] ->
+        raise (InvalidParameterType (1, "float"))
+    (* Other types *)
+    | [param1; _] -> raise (InvalidParameterType (2, type_of param1))
+    | other -> raise (InvalidNumberOfParameters (2, List.length other)) 
+  )
+;;
+
+_DEFINE_P_FUN "_div"
+  (function
+    (* Numeric division *)
+    | [EXP (Numeric (Int a)); EXP (Numeric (Int b))] -> 
+        EXP (Numeric (Int (a / b)))
+    | [EXP (Numeric (Int a)); EXP (Numeric (Float b))] ->
+        EXP (Numeric (Float ((float_of_int a) /. b)))
+    | [EXP (Numeric (Float a)); EXP (Numeric (Int b))] ->
+        EXP (Numeric (Float (a /. (float_of_int b))))
+    | [EXP (Numeric (Float a)); EXP (Numeric (Float b))] -> 
+        EXP (Numeric (Float (a /. b)))    
+    | [param1; _] -> raise (InvalidParameterType (2, type_of param1))
+    | other -> raise (InvalidNumberOfParameters (2, List.length other)) 
+  )
+;;
+
+_DEFINE_P_FUN "_mod"
+  (function
+    (* Numeric modulo *)
+    | [EXP (Numeric (Int a)); EXP (Numeric (Int b))] -> 
+        EXP (Numeric (Int (a mod b)))
+    | [EXP (Numeric (Int _)); _] -> 
+        raise (InvalidParameterType (2, "integer"))
+    | [_; _] -> raise (InvalidParameterType (1, "integer"))
+    | other -> raise (InvalidNumberOfParameters (2, List.length other)) 
+  )
+;;
+
+_DEFINE_P_FUN "_neg"
+  (function
+    (* Numeric negative *)
+    | [EXP (Numeric (Int a))] -> EXP (Numeric (Int (-a)))
+    | [EXP (Numeric (Float a))] -> EXP (Numeric (Float (a *. (-1.))))
+    | [param1] -> raise (InvalidParameterType (1, "numeric"))
+    | other -> raise (InvalidNumberOfParameters (1, List.length other)) 
+  )
+;;"[1;2] < [1;2]"
+
+(* Curried function generator for comparison expressions *)
+let _COMPARE_EXPR operator repr params = match params with
+  | [(DICT _) as a; b] | [b; (DICT _) as a] ->
+      (match repr with
+        | ">" | ">=" | "<" | "<=" ->
+          raise (NotSupportedTypeException (repr, "dict"))
+        | _ -> EXP (Boolean (operator a b))
+      )
+  | [NULL as a; b] | [b; NULL as a] ->
+      (match repr with
+        | ">" | ">=" | "<" | "<=" ->
+          raise (NotSupportedTypeException (repr, "null"))
+        | _ -> EXP (Boolean (operator a b))
+      )
+  | [a;b] -> EXP (Boolean (operator a b))
+  | other -> ( match List.length other with
+    | 2 -> raise (InvalidParameterType (2, type_of (List.nth other 1)))
+    | n -> raise (InvalidNumberOfParameters (2, List.length other))
+  )
+;;
+
+List.iter (function identifier, operator, repr ->
+    _DEFINE_P_FUN identifier (_COMPARE_EXPR operator repr)
+  )
+  [ "_eql", (=), "=="; "_neql", (<>), "!=";
+    "_gt", (>), ">"; "_lt", (<), "<";
+    "_gt_eq", (>=), ">="; "_lt_eq", (<=), "<="]
+;;
+
+(* Allows us to make boolean operations on expressions *)
+(* Just null and false values are treated like false   *)
+(* MAY CHANGE? *) 
+let _BOOL_OF_EXP = function
+  | NULL -> false
+  | EXP (Boolean b) -> b
+  | _ -> true  
+;;
+
+(* Curried method for boolean operations *)
+let _BOOL_OPERATOR_EXPR operator params = match params with
+  | [a;b] -> EXP (Boolean (operator (_BOOL_OF_EXP a) (_BOOL_OF_EXP b)))
+  | other -> ( match List.length other with
+	| 2 -> raise (InvalidParameterType (2, type_of (List.nth other 1)))
+	| n -> raise (InvalidNumberOfParameters (2, List.length other)))
+;;
+
+List.iter (function identifier, operator ->
+    _DEFINE_P_FUN identifier (_BOOL_OPERATOR_EXPR operator)
+  )
+  [ "_and", (&&); "_or", (||); "_xor", (<>)]
+;;
+
+_DEFINE_P_FUN "_not"
+  (function
+    | [exp] -> EXP (Boolean (not (_BOOL_OF_EXP exp)))
+    | other -> raise (InvalidNumberOfParameters (1, List.length other)) 
+  )
+;;
+
+_DEFINE_P_FUN "len" 
   (function
     | [EXP (String s)]   -> EXP (Numeric (Int (String.length s)))
     | [EXP (Char   s)]   -> EXP (Numeric (Int 1))
-    | [_]       -> raise (InvalidParameterType (1, "string or character"))
+    | [LIST l]           -> EXP (Numeric (Int (List.length l)))
+    | [DICT d]           -> EXP (Numeric (Int (List.length d)))
+    | [_]       -> raise (InvalidParameterType
+                  (1, "string or character or list or dict"))
     | other     -> raise (InvalidNumberOfParameters (1, List.length other)) 
   )
 ;;
@@ -87,9 +330,14 @@ _DEFINE_P_FUN "strlen"
 _DEFINE_P_FUN "_list_at"
   (function
     | [LIST l; EXP (Numeric (Int i))] ->
-      List.nth l i
+      (try List.nth l i with 
+        | Failure "nth" ->
+          raise (IndexOutOfBoundsException (LIST l, i, List.length l)))
     | [EXP (String s); EXP (Numeric (Int i))] ->
-      EXP (Char s.[i])
+      (try EXP (Char s.[i]) with
+        | Invalid_argument "index out of bounds" ->
+          raise (IndexOutOfBoundsException (EXP (String s), i, String.length s))
+      )
     | [_; EXP (Numeric (Int _))] ->
       raise (InvalidParameterType (1, "list or string"))
     | [_; _] ->
@@ -113,7 +361,7 @@ _DEFINE_P_FUN "_dict_at"
     | [_; EXP (String _)] ->
       raise (InvalidParameterType (1, "dictionary"))
     | [_; _] ->
-      raise (InvalidParameterType (2, "integer"))
+      raise (InvalidParameterType (2, "string"))
     | other ->
       raise (InvalidNumberOfParameters (2, List.length other))
   )
@@ -123,7 +371,6 @@ _DEFINE_P_FUN "_dict_at"
 (* *********************************************************** *)
 (* ****************** General evaluation ********************* *)
 (* *********************************************************** *)
-
 
 (* 
  * From a list of ocamlets, contained in an expression block in 
@@ -137,6 +384,10 @@ let eval_expression str =
   let results = rl ocamlets in
   List.hd (List.rev results)
 ;;
+
+let expand str =
+  let lexbuf = Lexing.from_string str in
+    parse_ocamlet second_level lexbuf;;
     
 (*
  * Printer for type expression
@@ -145,15 +396,21 @@ let rec string_of_expression expression =
   let string_of_expression_list lst =
     "[" ^ (String.concat "; " (List.map string_of_expression lst)) ^ "]" 
   in let string_of_expression_dict d =
-    "[" ^
+    "{" ^
       (String.concat "; " (List.map
         (function st, exp -> st ^ ":" ^ (string_of_expression exp))
         d
       )) ^
-    "]"
+    "}"
   in match expression with
   | EXP(Numeric(Int i))         -> string_of_int i
-  | EXP(Numeric(Float f))       -> string_of_float f
+  | EXP(Numeric(Float f))       ->
+    (* Add a 0 if the float has no decimal part (e.g. 10. ) *) 
+    let fstr = string_of_float f in
+      if fstr.[(String.length fstr) - 1] = '.' then
+        fstr ^ "0"
+      else
+        fstr
   | EXP(Boolean b)              -> string_of_bool b
   | EXP(Char c)                 -> string_of_char c
   | EXP(String s)               -> s
--- trunk/src/second_level_parser/grammar.mly
+++ trunk/src/second_level_parser/grammar.mly
@@ -58,12 +58,24 @@ and slp_ocamlet =
   /* Basic types operators */
 %token PLUS MINUS TIMES DIV MOD NEG
 %token AND OR XOR NOT
-%token EQL NOT_EQL GT LT GT_EQ LT_EQ CARET
+%token EQL NEQL GT LT GT_EQ LT_EQ CARET
   /* Control symbols */
 %token OPEN_PAR CLOSE_PAR OPEN_BRK CLOSE_BRK
 %token OPEN_SQRBRK CLOSE_SQRBRK
 %token COLON SEMICOLON DOT COMMA
 %token EOF
+
+  /* Precedence and association */
+  /* Lowest to highest */
+%left AND OR XOR
+%right NOT
+%left EQL NEQL GT LT GT_EQ LT_EQ
+%left PLUS MINUS
+%left TIMES DIV
+%left MOD CARET
+%right NEG
+
+  /* Entry point */
 %start parse_ocamlet
 %type <slp_ocamlet list> parse_ocamlet
 
@@ -76,20 +88,47 @@ parse_ocamlet: /* list of slp_ocamlet */
 ;
 
 expression: /* slp_ocamlet */
-    OPEN_PAR expression CLOSE_PAR              { $2 }
-  | funct                                      { $1 }
-  | basic_expression                           { $1 }
+    parenthesized_exp             { $1 }
+  | funct                         { $1 }
+  | basic_exp                     { $1 }
+  | elemental_type                { P_EXP $1 }  
 
-basic_expression: /* slp_ocamlet */
-  elemental_type                               { P_EXP $1 }
-;
+parenthesized_exp: /* slp_ocamlet */
+    OPEN_PAR expression CLOSE_PAR { $2 }
 
 funct: /* slp_ocamlet */
-	  IDENTIFIER OPEN_PAR list_contents CLOSE_PAR { P_FUN ($1, $3) }
-	| expression DOT OPEN_SQRBRK expression CLOSE_SQRBRK {
+  | IDENTIFIER OPEN_PAR list_contents CLOSE_PAR     { P_FUN ($1, $3) }
+	| single_exp DOT OPEN_SQRBRK expression CLOSE_SQRBRK {
 	            P_FUN ("_list_at", [$1; $4]) }
-	| expression DOT OPEN_BRK expression CLOSE_BRK {
+	| single_exp DOT OPEN_BRK expression CLOSE_BRK {
 	            P_FUN ("_dict_at", [$1; $4]) }
+
+/* Created to resolve shift/resolve conflict in functions */
+/* i.e., there was a shift/reduce conflict in 1 + [2].[3] */
+/* to (1 + [2]).[3] and 1 + ([2].[3]) for instance        */
+single_exp: /* slp_ocamlet */
+    parenthesized_exp               { $1 }
+  | funct                           { $1 }
+  | elemental_type                  { P_EXP $1 }
+                                    
+basic_exp: /* slp_ocamlet */
+    expression AND   expression   { P_FUN ("_and",   [$1; $3]) }
+  | expression OR    expression   { P_FUN ("_or",    [$1; $3]) }
+  | expression XOR   expression   { P_FUN ("_xor",   [$1; $3]) }
+  | NOT expression                { P_FUN ("_not",   [$2]    ) }
+	| expression EQL   expression   { P_FUN ("_eql",   [$1; $3]) }
+  | expression NEQL  expression   { P_FUN ("_neql",  [$1; $3]) }
+  | expression GT    expression   { P_FUN ("_gt",    [$1; $3]) }
+  | expression LT    expression   { P_FUN ("_lt",    [$1; $3]) }
+  | expression GT_EQ expression   { P_FUN ("_gt_eq", [$1; $3]) }
+  | expression LT_EQ expression   { P_FUN ("_lt_eq", [$1; $3]) }
+  | expression PLUS  expression   { P_FUN ("_plus",  [$1; $3]) }
+  | expression MINUS expression   { P_FUN ("_minus", [$1; $3]) }
+  | expression TIMES expression   { P_FUN ("_times", [$1; $3]) }
+	| expression DIV   expression   { P_FUN ("_div",   [$1; $3]) }
+  | expression MOD   expression   { P_FUN ("_mod",   [$1; $3]) }
+	| expression CARET expression   { P_FUN ("_caret", [$1; $3]) }
+  | NEG expression                { P_FUN ("_neg",   [$2])     }
 ;
 
 elemental_type: /* slp_expression */
@@ -102,17 +141,17 @@ list: /* slp_expression */
   OPEN_SQRBRK list_contents CLOSE_SQRBRK        { P_LIST $2 }
 ;
 
-dict: /* slp_expression */
-  OPEN_BRK dict_contents CLOSE_BRK              { P_DICT $2 }
-;
-
-list_contents: /* list of slp_expression */ 
+list_contents: /* list of slp_ocamlet */ 
 	  /* Empty */                                 { [] }
 	| expression                                  { [$1] }
 	| expression SEMICOLON list_contents          { $1::$3 }
 ;
   
-dict_contents: /* list of (string * slp_expression ) */
+dict: /* slp_expression */
+  OPEN_BRK dict_contents CLOSE_BRK              { P_DICT $2 }
+;
+
+dict_contents: /* list of (string * slp_ocamlet ) */
 	  /* Empty */                                 { [] }
 	| dict_tuple                                  { [$1] }
 	| dict_tuple SEMICOLON dict_contents          { $1::$3 }
@@ -121,160 +160,26 @@ dict_contents: /* list of (string * slp_expression ) */
 dict_tuple: /* (string * slp_expression) */
   string COLON expression                       { $1, $3 }
 ;
-  
+
 basic_type: /* slp_expression */
-	  numeric                                     { P_BASIC (Numeric $1) }
-	| literal                                     { P_BASIC $1 }
-	| boolean                                     { P_BASIC (Boolean $1) } 
+    numeric                    { P_BASIC (Numeric $1) }
+  | boolean                    { P_BASIC (Boolean $1) }  
+  | string                     { P_BASIC (String  $1) }
+  | char                       { P_BASIC (Char    $1) }
 ;
 
-numeric: /* slp_numeric */
-  numeric_times                                 { $1 }
+numeric:
+  NUMERIC    { $1 }
 ;
   
-numeric_times: /* slp_numeric */
-	  numeric_times TIMES numeric_plus   { numeric_operation $1 $3 ( * ) ( *. ) }
-	| numeric_times DIV numeric_plus     { numeric_operation $1 $3 (/) (/.) }
-	| numeric_times MOD numeric_plus     {
-	    match $1, $3 with
-	      | Int a, Int b ->
-	        Int( a mod b )
-	      | Float a, Int b ->
-	        Int( (int_of_float a) mod b )
-	      | Int a, Float b ->
-	        Int( a mod (int_of_float b) )
-	      | Float a, Float b ->
-	        Int( (int_of_float a) mod (int_of_float b) ) }
-	| NEG numeric_times {
-	    match $2 with
-	      | Int a       -> Int( a * -1 )
-	      | Float a     -> Float( a *. -1. ) }
-	| numeric_plus                        { $1 }
-;
-
-numeric_plus: /* slp_numeric */
-	  numeric_plus PLUS numeric_atom     { numeric_operation $1 $3 (+) (+.) }
-	| numeric_plus MINUS numeric_atom    { numeric_operation $1 $3 (-) (-.) }
-	| numeric_atom                       { $1 }
-;
-  
-numeric_atom: /* slp_numeric */
-  NUMERIC                              { $1 }
-;
-
-boolean: /* boolean */
-  boolean_op                           { $1 }
-;
-
-boolean_op: /* boolean */
-	  boolean_op OR boolean_eq           { $1 || $3 }
-	| boolean_op AND boolean_eq          { $1 && $3 }
-	| boolean_op XOR boolean_eq          { $1 <> $3 }
-	| NOT boolean_op                     { not $2 }
-	| boolean_eq                         { $1 }
-;
-
-boolean_eq: /* boolean */
-	  boolean_eq EQL boolean_comp        { $1 = $3 }
-	| boolean_eq NOT_EQL boolean_comp    { $1 <> $3 }
-  | boolean_comp                       { $1 }
-;
-
-boolean_comp: /* boolean */
-  | comparable GT_EQ comparable {
-    match $1, $3 with 
-		  | Numeric(Int a), Numeric(Int b)      -> a = b
-		  | Numeric(Float a), Numeric(Float b)  -> a = b
-			| String a, String b                  -> a = b
-			| Char a, Char b                      -> a = b
-		  | Numeric(Int a), Numeric(Float b)    ->
-		    (float_of_int a) = b
-		  | Numeric(Float a), Numeric(Int b)    ->
-		    a = (float_of_int b)
-			| String a, Char b                    ->
-			  a = (string_of_char b)
-			| Char a, String b                    ->
-			  (string_of_char a) = b
-			| _ -> raise TypeMismatchError}
-	| comparable LT_EQ comparable {
-    match $1, $3 with 
-		  | Numeric(Int a), Numeric(Int b)      -> a <= b
-		  | Numeric(Float a), Numeric(Float b)  -> a <= b
-			| String a, String b                  -> a <= b
-			| Char a, Char b                      -> a <= b
-		  | Numeric(Int a), Numeric(Float b)    ->
-		    (float_of_int a) <= b
-		  | Numeric(Float a), Numeric(Int b)    ->
-		    a <= (float_of_int b)
-			| String a, Char b                    ->
-			  a <= (string_of_char b)
-			| Char a, String b                    ->
-			  (string_of_char a) <= b
-			| _ -> raise TypeMismatchError}
-  | comparable GT comparable    {
-    match $1, $3 with 
-		  | Numeric(Int a), Numeric(Int b)      -> a >= b
-		  | Numeric(Float a), Numeric(Float b)  -> a >= b
-			| String a, String b                  -> a >= b
-			| Char a, Char b                      -> a >= b
-		  | Numeric(Int a), Numeric(Float b)    ->
-		    (float_of_int a) >= b
-		  | Numeric(Float a), Numeric(Int b)    ->
-		    a >= (float_of_int b)
-			| String a, Char b                    ->
-			  a >= (string_of_char b)
-			| Char a, String b                    ->
-			  (string_of_char a) >= b
-			| _ -> raise TypeMismatchError}
-	| comparable LT comparable    {
-    match $1, $3 with 
-		  | Numeric(Int a), Numeric(Int b)      -> a < b
-		  | Numeric(Float a), Numeric(Float b)  -> a < b
-			| String a, String b                  -> a < b
-			| Char a, Char b                      -> a < b
-		  | Numeric(Int a), Numeric(Float b)    ->
-		    (float_of_int a) < b
-		  | Numeric(Float a), Numeric(Int b)    ->
-		    a < (float_of_int b)
-			| String a, Char b                    ->
-			  a < (string_of_char b)
-			| Char a, String b                    ->
-			  (string_of_char a) < b
-			| _ -> raise TypeMismatchError}
-  | boolean_atom                { $1 }
-
-comparable: /* slp_basic_expression */
-    numeric                              { Numeric $1 }
-  | string                               { String $1 }
-  | char                                 { Char $1 }
-  | boolean_atom                         { Boolean $1 }
-;
-
-boolean_atom: /* boolean */
-  BOOLEAN                                { $1 }
-;
-  
-literal: /* slp_basic_type */
-	  literal CARET literal_atom {
-	    match $1, $3 with
-	      | String s, String t ->
-	        String( s ^ t )
-	      | String s, Char t ->
-	        String( s ^ (string_of_char t))
-	      | Char s, String t ->
-	        String( (string_of_char s) ^ t )
-        | _ -> raise TypeMismatchError
-	  }
-  | literal_atom  { $1 }
-;
-
-literal_atom: /* slp_basic_type */
-    string                  { String $1 }
-  | char                    { Char $1   }
+boolean:
+  BOOLEAN    { $1 }
 ;
 
 string:
   STRING     { $1 }
-
+;
+  
 char:
   CHAR       { $1 }
+;
