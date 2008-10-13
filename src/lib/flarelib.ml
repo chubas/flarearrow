e589f26aab10ea02ae3f16ad683ab200cf2f09b8
chubas7 <chubas7@e7b0d6f7-5054-0410-bf6d-e59bcb6200f7>
2008-10-13 05:11:15 +0000

    Changed file_to_string method to not add the extra newline at the end of the file; and removed duplicate method file_to_stringf

--- src/lib/flarelib.ml
+++ src/lib/flarelib.ml
@@ -16,17 +16,23 @@ let (++) str chr =
 let list_to_string list = 
   List.fold_left (++) "" list
 
-(* Read line by line of the file in an expandable buffer and return the file*)
+(* Read line by line of the file in an expandable buffer and return the file *)
 (* contents in a string *)
 let file_to_string channel =
   let buf = Buffer.create 4096 in
+  let sep = ref "" in
   let rec loop () = 
     (* Read a line *)
     let line = input_line channel in
+    
+    (* Put the newline character (or nothing if it is the first line) *)
+    (* in the buffer *)
+    Buffer.add_string buf !sep;
+    
     (* Put the line in the buffer *)
     Buffer.add_string buf line;
-    (* Put the newline character in the buffer *)
-    Buffer.add_char buf '\n';
+    
+    sep := "\n";
     (* Continue reading *)
     loop ()
     in
@@ -39,16 +45,5 @@ let file_to_string channel =
       End_of_file -> Buffer.contents buf
 ;;
 
-let file_to_stringf filename =
-		let buf = Buffer.create 2048 in
-	let rec print_all_lines in_chan =
-		Buffer.add_string buf ((input_line in_chan)^"\n");
-		print_all_lines in_chan
-	in
-	let in_file = open_in filename in
-	try
-		print_all_lines in_file
-	with End_of_file -> 
-		close_in in_file;
-		Buffer.contents buf
-	;;
+let file_to_stringf filename = file_to_string (open_in filename);;
+
