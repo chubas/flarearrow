30e0524016719673a491a9b049229c2bcd8eb0c8
chubas7 <chubas7@e7b0d6f7-5054-0410-bf6d-e59bcb6200f7>
2008-10-18 21:52:21 +0000

    Created a makefile and makefiles for each subdir. Each time we create an iteration, should update the makefile and create makefiles in the subdirectories. Added the test for web server and some test files. RIGHT NOW IT DOESN'T SUPPORT POST, so it won't pass all the tests

--- trunk/src/tests/test_all.ml
+++ trunk/src/tests/test_all.ml
@@ -1,2 +1,4 @@
 Test_first_level_parser.test ();;
+print_endline "";;
 Test_expression_evaluator.test ();;
+print_endline "";;
---
+++ trunk/src/tests/test_web_server.py
@@ -0,0 +1,63 @@
+import unittest
+import socket
+
+def test_connection(method):
+  def connect(host = 'localhost', port = 5000, resource = 'test_file.html'): 
+    try:
+      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      sock.connect((host, port))
+      sock_out = sock.makefile('wb')
+      sock_in = sock.makefile('r')
+      sock.close()
+      print >> sock_out, method + ' /' + resource + ' HTTP/1.0\n\n'
+      sock_out.close()
+      result = sock_in.read()
+      sock_in.close()
+      return result
+    except socket.error:
+      print "\n(!) Could not connect to " + host + ":" + str(port) + \
+            " for testing (" + method + ").\n" + \
+            "\tMake sure the server is correctly configured and running."
+      return ""
+  return connect
+
+test_connection_get     = test_connection("GET")
+test_connection_post    = test_connection("POST")
+
+def readfile(filename):
+  f = open(filename, 'r')
+  contents = f.read()
+  f.close()
+  return contents
+  
+
+class ClientTest(unittest.TestCase):
+  def test_http_get(self):
+    code = "HTTP/1.1 200 OK"
+    content_type = "Content-type: text/html"
+    content_length = "Content-Length: 416"
+    server = "Server: Ocamlnet/2.2.8.1"
+    contents = readfile("../../public/test_file.html")
+    response = test_connection_get()
+
+    self.assert_(code in response)
+    self.assert_(content_type in response)
+    self.assert_(content_length in response)
+    self.assert_(server in response)
+    self.assert_(contents in response)
+
+  def test_http_post(self):
+    code = "HTTP/1.1 200 OK"
+    content_type = "Content-type: text/html"
+    content_length = "Content-Length: 416"
+    server = "Server: Ocamlnet/2.2.8.1"
+    contents = readfile("../../public/test_file.html")
+    response = test_connection_post()
+
+    self.assert_(code in response)
+    self.assert_(content_type in response)
+    self.assert_(content_length in response)
+    self.assert_(server in response)
+    self.assert_(contents in response)
+
+unittest.main()
