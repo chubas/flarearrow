9b422d1220d81e67aaae2863584e06be1312f041
chubas7 <chubas7@e7b0d6f7-5054-0410-bf6d-e59bcb6200f7>
2008-10-31 05:40:48 +0000

    Added numbers test

--- src/web_server/server.ml
+++ src/web_server/server.ml
@@ -30,11 +30,45 @@ let headers_plain_text (cgi:cgi_activation) =
   ()
 ;;
 
+let numbers_controller_params (cgi:cgi_activation) =
+  let num1 = get_variable_or_default cgi "num1" ~default:None in
+  let num2 = get_variable_or_default cgi "num2" ~default:None in
+  let rec fact_rec acum n = match n with
+    | 1 -> acum
+    | e -> fact_rec (acum * n) (n - 1) in
+  let fact = fact_rec 1 in
+  let sum a = (a * (a + 1)) / 2 in
+  let rec gcd a b = match b with
+    | 0 -> a
+    | _ -> gcd b (a mod b) in
+   match num1, num2 with
+    | Some a, Some b ->
+      let a, b = (int_of_string a), (int_of_string b) in
+      let f = fact a in
+      let s = sum b in
+      let g = gcd a b in
+      let env = cgi # environment in
+      let fields = env # input_header_fields in
+      let concat s (e1, e2) = s ^ e1 ^ ": " ^ e2 ^ "<br />" in  
+      let headers = List.fold_left concat "" fields in
+      [
+        "num1", EXP( Numeric (Int a));
+        "num2", EXP( Numeric (Int b));
+        "fact", EXP( Numeric (Int f));
+        "sum", EXP( Numeric (Int s));
+        "gcd", EXP( Numeric (Int g));
+        "headers", EXP (String headers)
+      ]
+    | _, _ -> failwith "Required parameters num1 and num2"
+;;
+
 Server_utils.start 
   [
 	  template_process "hello_world";
 	  template_process "oper"
       ~bind_parameters:op_controller_params;
     template_process "headers"
-      ~headers:headers_plain_text
+      ~headers:headers_plain_text;
+    template_process "numbers"
+      ~bind_parameters:numbers_controller_params
   ]
