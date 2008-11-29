a04eabf9b13e6310b105f946924585f3f2fc6fe0
chubas7 <chubas7@e7b0d6f7-5054-0410-bf6d-e59bcb6200f7>
2008-11-29 01:32:19 +0000

    Application working\!

--- trunk/src/web_server/client_lib.ml
+++ trunk/src/web_server/client_lib.ml
@@ -37,8 +37,8 @@ let user_authenticates db_connection username password =
       | 1L -> true
       | _  -> raise (DuplicatedUser username)
 ;;
-  
-let user_answered db_connection username is_correct =
+
+let user_stats db_connection username =
   let (current_tries, current_corrects) = 
     let result = Mysql.exec db_connection
       ("select tries, corrects from users where username like binary " ^ 
@@ -49,8 +49,12 @@ let user_answered db_connection username is_correct =
         | [Some _tries; Some _corrects] -> 
             (int_of_string _tries, int_of_string _corrects)
         | _ -> assert false
-      )  
-  in
+      ) in
+  (current_tries, current_corrects)
+;;
+  
+let user_answered db_connection username is_correct =
+  let (current_tries, current_corrects) = user_stats db_connection username in
   let new_tries = current_tries + 1 in
   let new_corrects = current_corrects + (if is_correct then 1 else 0) in
   ignore (
@@ -60,7 +64,7 @@ let user_answered db_connection username is_correct =
      " where username = " ^ (Mysql.ml2str username)));
     (new_tries, new_corrects)
 ;;
-
+  
 let print_query db_connection query = 
 	let result = Mysql.exec db_connection query in
 		Mysql.iter result
@@ -114,13 +118,77 @@ let word_list =
   (* List.map (Netencoding.Url.encode) *) 
   (* Encoding should be handler by the javascript? *)
   [
+    "apple";"baby";"back";"ball";"bear";"bed";"bell";"bird";
+    "birthday";"boat";"box";"boy";"bread";"brother";"cake";
+    "car";"cat";"chair";"chicken";"children";"christmas";
+    "coat";"corn";"cow";"day";"dog";"doll";"door";"duck";"egg";
+    "eye";"farm";"farmer";"father";"feet";"fire";"fish";
+    "floor";"flower";"game";"garden";"girl";"goodbye";
+    "grass";"ground";"hand";"head";"hill";"home";"horse";
+    "house";"kitty";"leg";"letter";"man";"men";"milk";"money";
+    "morning";"mother";"name";"nest";"night";"paper";"party";
+    "picture";"pig";"rabbit";"rain";"ring";"santa claus";
+    "school";"seed";"sheep";"shoe";"sister";"snow";"song";
+    "squirrel";"stick";"street";"sun";"table";"thing";"time";
+    "top";"toy";"tree";"watch";"water";"way";"wind";"window";
+    "wood"; "zebra"; "chocolate"; "candy"; "gamer";
     "color"; "computer"; "videogame"; "stomach";
-    "carrot"; "dog"; "pancake"; "snake";
+    "carrot"; "pancake"; "snake"; "desktop";
     "spoon"; "world"; "pacman"; "magazine";
     "big ben"; "rolling stones"; "spongebob";
     "lion"; "hot dog"; "soccer"; "south park";
     "mario bros"; "scotland"; "frog"; "windows";
-    "blue"; "red"; "television"; "children";
+    "blue"; "red"; "yellow"; "green"; "black"; "pink";
+    "television"; "children"; "radio"; "triangle";
     "kinder"; "ferrari"; "paperclip"; "bush";
-    "scissors"; "pokemon"; "blackberry"; "youtube" 
+    "scissors"; "pokemon"; "blackberry"; "youtube";
+    "blog"; "lifehacker"; "train"; "star wars";
+    "fox"; "monkey"; "ice cream"; "zoo"; "explosion";
+    "doll"; "skyscrapper"; "rock"; "lunch"; "rocket";
+    "shirt"; "batman"; "spiderman"; "x-men"; "watchmen";
+    "japan"; "mexico"; "india"; "china"; "australia";
+    "netherlands"; "germany"; "russia"; "spain"; "wtf";
+    "coffee"; "milk"; "jesus"; "alcohol"; "glass"; "plate";
+    "letters"; "firefighter"; "astronaut"; "vodka";
+    "screen"; "scream"; "cry"; "laugh"; "fall"; "smile";
+    "run"; "driver"; "motorcycle"; "bus"; "bell"; "tire";
+    "motor"; "insect"; "box"; "domo"; "anime"; "backpack";
+    "schoolbag"; "ruler"; "pencil"; "pen"; "elephant";
+    "kangaroo"; "giraffe"; "lettuce"; "drink"; "fireworks";
+    "laptop"; "pear"; "cigar"; "orange"; "juice";
+    "pinneapple"; "pizza"; "breakfast"; "clock";
+    "metal"; "wood"; "sleep"; "big"; "star"; "sun";
+    "moon"; "jupiter"; "mars"; "ball"; "sunset"; "rainbow";
+    "gold"; "honey"; "bee"; "ant"; "hive"; "knife";
+    "fork"; "saw"; "jigsaw"; "controller"; "transformers";
+    "brush"; "paint"; "minesweeper"; "blackjack";
+    "chess"; "knight"; "queen"; "princess"; "king";
+    "toadstool"; "peach"; "random"; "think"; "tag";
+    "brain"; "goal"; "tennis"; "basketball"; "swim";
+    "NullPointerException"; "swan"; "duck"; "eagle";
+    "platipus"; "key"; "keychain"; "yoshi"; "ring";
+    "people"; "cell phone"; "telephone"; "crayon";
+    "basket"; "sushi"; "rice"; "taco"; "burrito";
+    "sweater"; "pants"; "socks"; "trousers"; "underwear";
+    "nail"; "hammer"; "shovel"; "farm"; "train";
+    "storm"; "cloud"; "class"; "shower"; "music";
+    "piano"; "guitar"; "violin"; "fire"; "water";
+    "bass"; "kitchen"; "cookie"; "table"; "sesame street";
+    "monster"; "devil"; "witch"; "ghost"; "vampire";
+    "werewolf"; "wolf"; "mozart"; "beethoven"; "heart";
+    "dance"; "einstein"; "newton"; "physics"; "atom";
+    "flickr"; "google"; "yahoo"; "microsoft";
+    "facebook"; "spam"; "worm"; "mail"; "movie";
+    "mask"; "chair"; "iron"; "obama"; "iron man";
+    "hair"; "ear"; "finger"; "foot"; "root";
+    "fountain"; "lake"; "gnome"; "waterfall"; "rain";
+    "forest"; "gorilla"; "jungle"; "gun"; "rose";
+    "church"; "turing"; "thesis"; "work"; "warcraft";
+    "artist"; "france"; "cheese"; "mouse"; "moose";
+    "horn"; "unicorn"; "siren"; "sea"; "shell";
+    "squid"; "fish"; "bubble"; "starfish"; "shark";
+    "whale"; "dragon ball"; "fight"; "blood";
+    "street"; "thumb"; "boat"; "road"; "flag";
+    "golf"
   ]
+;;
